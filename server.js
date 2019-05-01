74175c673009c17158a86b673e73f772d1834c21
N4n3x <alex.hern.dev@gmail.com>
2019-05-01 17:14:09 +0200

    fix

--- server.js
+++ server.js
@@ -25,6 +25,12 @@ db.once('open', function (){
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(function(req, res, next){
   if(req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT'){
     jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', function(err, decode){
