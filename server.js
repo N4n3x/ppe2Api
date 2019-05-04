39521cb4d42f11ba789cd2755afcfcd7bbacf37e
N4n3x <alex.hern.dev@gmail.com>
2019-05-05 00:01:30 +0200

    fix

--- server.js
+++ server.js
@@ -29,6 +29,10 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  if (req.method === "OPTIONS") {
+    console.log("!OPTIONS");
+    res.end();
+  }
   next();
 });
 
